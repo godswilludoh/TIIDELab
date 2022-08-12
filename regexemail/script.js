22748c6370de38d09bf6ac73efc5b1d463506b22
Godswill Udoh <godswilludoh@gmail.com>
2022-08-12 20:38:19 +0100

    added changes

--- regexemail/script.js
+++ regexemail/script.js
@@ -1,12 +1,16 @@
-const email = 'godswilludoh@gmail.com';
+/** Regex Validate Mail */
 
-alert('Welcome Client!');
-let emailInput = prompt('Please input your email');
+function validateEmail() {
+	alert('Welcome Client!');
 
-if (emailInput == email) {
-	const replaceEmail = email.replace(
-		/(\w{3})[\w.-]+@([\w.]+\w)/,
-		'*************'
-	);
-	console.log(replaceEmail);
+	let email = prompt('Please input a valid email address');
+	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+		const replaceEmail = email.replace(/(\w{3})[\w.-]+@([\w.]+\w)/, '$1***@$2');
+		alert(replaceEmail);
+	} else if (email == '') {
+		alert('Please input your email address');
+	} else {
+		alert('You have entered an invalid email address!');
+	}
 }
+validateEmail();
